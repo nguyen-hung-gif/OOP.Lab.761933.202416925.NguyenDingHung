20683e1deda19293ec98e743cfaff1bffbb0ea15
nguyen-hung-gif <hn7731455@gmail.com>
2026-03-24 14:03:13 +0700

    add helloworld

---
+++ Lab01/HelloWorld.java
@@ -0,0 +1,12 @@
+package oop_lab;
+
+import javax.swing.JOptionPane;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog (null, "Hello world! How are you?");
+		System.exit(0);
+	}
+
+}
